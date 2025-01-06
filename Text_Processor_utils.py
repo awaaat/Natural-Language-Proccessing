ae6fe3493b2dd71dc78e4e151c82396c1a5b74a6
AllanD <writers.smith112@gmail.com>
2025-01-06 16:02:09 +0300

    modified the text processor utils file. Done some data NLP Data cleaning exercises

--- Text_Processor_utils.py
+++ Text_Processor_utils.py
@@ -26,10 +26,37 @@ def read_online_text_data(text_url):
     """
     with urllib.request.urlopen(url = text_url) as f:
         return f.read().decode('ISO-8859-1')
+    
+#We can also read the text data from the local machine
+def read_local_text_data(file_path):
+    """
+    Reads the contents of a text file from a local path.
 
-def remove_punctuations(col):
+    Args:
+    ----------
+    file_path : str
+        The path to the text file on the local machine.
+
+    Returns:
+    -------
+    str
+        The full text content of the file as a string.
+
+    Raises:
+    ------
+    FileNotFoundError
+        If the specified file path does not exist.
+    IOError
+        If there is an error reading the file.
+    """
+    with open(file_path, 'r', encoding= 'utf-8') as file:
+        text_data = file.read()
+    return text_data
+
+def remove_punctuations(words):
     """
     Remove all punctuation characters from the input string.
+
     Args:
         col (str): A string from which punctuation characters should be removed.
     Returns:
@@ -38,6 +65,31 @@ def remove_punctuations(col):
         >>> remove_punctuations("Hello, World!")
         'Hello World'
     """
-    return ''.join(words for words in col if words not in string.punctuation)
+    words  = words.lower()
+    return ''.join([word for word in words if word not in string.punctuation])
+
+def bag_of_words_count(words:list, word_dict= {}):
+    """
+    Counts the occurrences of each word in a given list and returns a dictionary 
+    with the word counts.
+
+    Args
+    ----------
+    words : list
+        A list of words to count.
+    word_dict : dict, optional
+        An existing dictionary to update with word counts (default is an empty dictionary).
+
+    Returns
+    -------
+    dict
+        A dictionary where keys are words and values are their corresponding counts.
+    """
+    for word in words:
+        if word in word_dict.keys():
+            word_dict[word] += 1
+        else:
+            word_dict[word]= 1
+    return word_dict
 
     
